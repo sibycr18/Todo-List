f861e9a9b0997ab7a96c64e1ab73c3d0bdf7475f
Siby C R <sibycr18@gmail.com>
2024-04-10 11:21:19 +0530

    intialised db

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 from flask import Flask
+from pymongo import MongoClient
 
 app = Flask(__name__)
+
+client = MongoClient()
+
+db = client['To-do list']
+collection = db['tasks']
+
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/add/task', methods=['POST'])
+def add_task():
+    pass
 
 if __name__ == '__main__':
     app.run()
