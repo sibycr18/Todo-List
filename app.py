9e4d222d1e2c2cf3d7f6e0fd4a88c00f9170fe50
Midhun Sankar J <midhunsankarj@gmail.com>
2024-04-10 11:50:17 +0530

    Created an Endpoint

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, session, request 
 from pymongo import MongoClient
 
 app = Flask(__name__)
@@ -15,7 +15,15 @@ def hello_world():
 
 @app.route('/add/task', methods=['POST'])
 def add_task():
-    pass
+    data = request.get_json()
+    task = data['task']
+    completed = data['completed']
+    _id = collection.insert_one({'task' : task, 'completed' : completed})
+    if _id:
+        response_data = {'success': True, 'message': 'Task added successfully', 'task_id': _id}
+        return Response(response_data)
+    response_data = {'success': False}
+    return Response(response_data)
 
 if __name__ == '__main__':
     app.run()
